8d346b12e6b4418b908f000f136fd6aa87d79d8e
selik <mattbrydg@gmail.com>
2022-02-20 21:12:19 -0700

    Title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# Returns a full title on a per page basis
+  def full_title(page_title = ' ')
+    base_title = "Orbit Feedback Application"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
